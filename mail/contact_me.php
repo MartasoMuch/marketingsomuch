2dcb693c9cc2fa9dd5ad30169d6eb364f88b3710
danielblacha <daniel.blacha@gmail.com>
2017-09-06 23:23:35 +0200

    fix email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -19,7 +19,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = 'marketingsomuch@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Strona MarketingSoMuch :  $name";
 $email_body = "Dostałaś wiadomość za pośrednictwem formularza na stronie MarktenigSoMuch.\n\n"."Treść wiadomości:\n\nImię: $name\n\nEmail: $email_address\n\nNr telefonu: $phone\n\nWiadomość:\n$message";
-$headers = "Od: MarketingSoMuch\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "marketingsomuch@gmail.com"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Odpowiedz: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
